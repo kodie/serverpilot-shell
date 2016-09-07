a47e597930f4a966a77a6de0af0aa6b4da4deecb
Kodie Grantham <kodie.grantham@gmail.com>
2016-09-07 11:29:50 -0500

    Don't display cli info as error

--- lib/serverpilot.sh
+++ lib/serverpilot.sh
@@ -530,7 +530,7 @@ function serverpilot {
     "sysusers") sp_sysusers "${@:2}";;
     "apps") sp_apps "${@:2}";;
     "dbs") sp_dbs "${@:2}";;
-    *) echo "ServerPilot API Shell Wrapper by Kodie Grantham" >&2; exit 1;;
+    *) echo "ServerPilot API Shell Wrapper by Kodie Grantham"; exit 0;;
   esac
 }
 
